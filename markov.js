ed3aeba4d8dd4a77df4f719cd94f8b14c634b1e1
Ryan Park <ryrypark@gmail.com>
2023-05-09 16:40:50 -0700

    implement getChains and getText for markov machine

--- markov.js
+++ markov.js
@@ -9,13 +9,13 @@ class MarkovMachine {
     // A "word" will also include any punctuation around the word, so this will
     // include things like "The", "cat", "cat.".
     this.words = text.split(/[ \r\n]+/);
-    this.chains = this.getChains();
+    this.chains = this.getChains(this.words);
   }
 
   /** Get markov chain: returns object of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,22 +23,58 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
-  getChains() {
-    // TODO: implement this!
+  getChains(words) {
+    const markovChain = {};
+
+    for (let i = 0; i < words.length; i++) {
+      let currentWord = words[i];
+      let nextWord = words[i+1];
+
+      if (i === words.length - 1) {
+        markovChain[currentWord] ? markovChain[currentWord].push(null) : markovChain[currentWord] = [null]
+      } else {
+        markovChain[currentWord] ? markovChain[currentWord].push(nextWord) : markovChain[currentWord] = [nextWord];
+      }
+    }
+
+    return markovChain
   }
 
+  /** Pick random choice from array. */
+  static choice(items) {
+    return Math.floor(Math.random() * items.length);
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
   getText() {
-    // TODO: implement this!
-
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+    const chain = this.chains;
+    let currentWord = this.words[0];
+    let resultText = currentWord;
+
+    while (currentWord !== null) {
+      let randomNum = MarkovMachine.choice(chain[currentWord])
+      let wordToAdd = chain[currentWord][randomNum];
+
+      if (wordToAdd === null) {
+        break;
+      }
+      resultText += (' ' + wordToAdd);
+
+      currentWord = wordToAdd;
+    }
+
+    return resultText
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
